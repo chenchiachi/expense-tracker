2c39ae21d1e4cddda04a332172e8cbfecb95b8cb
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-08-27 22:54:51 +0800

    Add mongoose

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+require('./config/mongoose')
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost/expense'
+
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+module.exports = db
