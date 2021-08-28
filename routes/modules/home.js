41bad374c7f3921cf7622688e1c6688d251ee4e2
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-08-29 01:21:44 +0800

    Modify index.handlebars

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,11 +1,27 @@
 const express = require('express')
-const record = require('../../models/record')
+const dayjs = require('dayjs')
 const router = express.Router()
 
 const Record = require('../../models/record')
+const CATEGORY = {
+  家居物業: "fas fa-home",
+  交通出行: "fas fa-shuttle-van",
+  休閒娛樂: "fas fa-grin-beam",
+  餐飲食品: "fas fa-utensils",
+  其他: "fas fa-pen"
+}
 
 router.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => {
+      records.forEach(record => {
+        record.icon = CATEGORY[record.category]
+        record.date = dayjs(record.date).format('YYYY-MM-DD')
+      })
+     res.render('index', { records })
+    })
+    .catch(error => console.error(error))
 })
 
 module.exports = router
