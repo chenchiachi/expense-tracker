472d03cf0680e6c6ddf3da28cfc0bb1171809d06
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-10-27 14:30:48 +0800

    Add user login page

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.engine('handlebars', exphbs({
 }))
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
