fd409b647a323a0f28d59285c00d50fc3fa344e8
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-10-27 22:12:15 +0800

    Add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -7,11 +7,25 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', 
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/users/login'
-}))
+
+router.post('/login', function (req, res, next) {
+  const { name, password } = req.body
+  if (!name || !password) {
+    req.flash('warning_msg', '請輸入使用者姓名和密碼。')
+    return res.redirect('/users/login')
+  }
+  passport.authenticate('local', function (err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { 
+      req.flash('warning_msg', info.message)
+      return res.redirect('/users/login')
+     }
+    req.logIn(user, function (err) {
+      if (err) { return next(err) }
+      return res.redirect('/')
+    })
+  })(req, res, next)
+})
 
 router.get('/register', (req, res) => {
   res.render('register')
@@ -19,9 +33,24 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !password || !confirmPassword) {
+    errors.push({message: '所有欄位皆是必填。'})
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不符。'})
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ name }).then(user => {
     if (user) {
-      console.log('User already exists.')
+      errors.push({ message: '此使用者已註冊。' })
       res.render('register', {
         name,
         password,
@@ -39,6 +68,7 @@ router.post('/register', (req, res) => {
 })
 router.get('/logout', (req,res) => {
   req.logout()
+  req.flash('success_msg', '已登出。')
   res.redirect('/users/login')
 })
 
