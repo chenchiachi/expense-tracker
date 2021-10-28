4070a23fc28709bb4e4d0ba08e925e4cc29fc671
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-10-28 10:42:58 +0800

    Modify heroku enviroment

--- config/passport.js
+++ config/passport.js
@@ -28,7 +28,7 @@ module.exports = app => {
   passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_ID,
     clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    callbackURL: process.env.FACEBOOK_CALLBACK,
     profileFields: ['displayName']
   }, (accessToken, refreshToken, profile, done) => {
     const name = profile._json.name
